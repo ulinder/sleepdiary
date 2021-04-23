92d72ac2e5fcc69eced4d8bff43e6482d5f27bdb
StinaKilborn <49522006+StinaKilborn@users.noreply.github.com>
2021-04-23 16:00:15 +0200

    Fixed crash on empty dbresults

--- routes/index.js
+++ routes/index.js
@@ -5,32 +5,32 @@ var db = require('../db.js');
 var moment = require('moment'); //.locale('sv');
 
 var date_table = (dbresults) =>{
-  if(!dbresults) return [];
+  if(!dbresults || dbresults.length === 0) return [];
   // create a table of dates between first and last post
-  // TABLE 
+  // TABLE
   // date, down, up, rate, sleep_length, sleep_efficiency, awake_time
-  // 
-
-  var first_date = dbresults[0].date, 
+  //
+  
+  var first_date = dbresults[0].date,
       last_date = dbresults[dbresults.length -1].date,
       current_date,
       table = [],
       found;
-  
+
   for (var i=0; i < 60; i++) {
 
       current_date = moment( dbresults[0].date ).add(i, 'days').format("YYYY-MM-DD");
 
       found = dbresults.find( (row)=> row.date == current_date);
-      found = found ? 
+      found = found ?
         [current_date, found.down, found.up, found.rate] : [current_date, "","","", "","",""];
 
       table.push( found );
-      if(current_date == moment(last_date).format("YYYY-MM-DD")) break; 
+      if(current_date == moment(last_date).format("YYYY-MM-DD")) break;
   }
   console.log(table);
   return table;
-}       
+}
 
 
 /* GET home page. */
@@ -45,21 +45,20 @@ router.get('/', (req, res, next) => {
 
 /* HANDLE login */
 router.get('/:id/:hash', (req, res, next) => {
- 
+
     db.get("SELECT * FROM users WHERE id=?", [req.params.id], (err, user) =>{
       console.log("User: ", user);
       if(user && user.hash == req.params.hash){
         res.cookie('user', req.params.id);
         res.cookie('user_hash', req.params.hash);
-        res.redirect('/');      
+        res.redirect('/');
       } else {
         res.render('401')
       }
-    
-    });    
+
+    });
 });
 
 
 
 module.exports = router;
-
