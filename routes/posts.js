6bb55ceb8e7c636aeeee77dd595e9e456ec9f2f0
Johnny Ulinder <julinder@gmail.com>
2021-06-14 17:10:50 +0200

    pack of fixes
    medel sovtid i en graf
    visa dagnamn i tabell
    addera timestamp till tabell
    namnändringar i tabeller
    06-buggen

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,9 @@ var db = require('../db.js');
 var moment = require('moment'); //.locale('sv');
 moment.locale('sv');
 
-var seconds_to_text = (time) => {
+const arrSum = (accumulator, currentValue) => accumulator + currentValue;  
+
+const seconds_to_text = (time) => {
     var hrs = ~~(time / 3600);
     var mins = ~~((time % 3600) / 60);
     var ret = "";
@@ -15,7 +17,7 @@ var seconds_to_text = (time) => {
     return ret;
 }
 
-var seconds_to_block_of = (type, seconds)=>{ // type => [h/m]
+const seconds_to_block_of = (type, seconds)=>{ // type => [h/m]
   if(type.match(/h|m/)){
     var hrs = ~~(seconds / 3600);
     var mins = ~~((seconds % 3600) / 60);
@@ -24,17 +26,20 @@ var seconds_to_block_of = (type, seconds)=>{ // type => [h/m]
   console.error('Fail in seconds to block');
 }
 
-var data_table = (dbresults) =>{
+const data_table = (dbresults) =>{
   if(!dbresults || dbresults.length == 0) return [];
   
   var first_date_str = moment( dbresults[0].up, "X" ).format("YYYY-MM-DD").toString(),
       first_week_str = moment( dbresults[0].up, "X" ).format("ww").toString(),
       this_up_date = first_date_str,
       posts_table = [],
-      weeks = new Map(),
+      weeks = {},
+      week_num,
       found,
       seconds_in_bed,
       seconds_asleep,
+      se,
+      week_arr = [],
       i = 0,
       _dummy = { id: "", date_to_bed: "", time_to_bed: "", date_up_from_bed: "", time_up_from_bed: "", time_in_bed: "", sleep_rate: "", time_awake: "", time_asleep: "", sleep_efficiency: ""};
 
@@ -50,9 +55,12 @@ var data_table = (dbresults) =>{
         found = dbresults.shift();
 
         seconds_in_bed = found.up - found.down;
-        seconds_asleep = seconds_in_bed - found.awake
+        seconds_asleep = seconds_in_bed - found.awake;
+        week_num = moment(this_up_date).format("ww");
+        se = seconds_asleep/seconds_in_bed*100
+
         posts_table.push( {
-          week: moment(this_up_date).format("ww"),
+          week: week_num,
           day: this_up_date, 
           found: true, 
           data: { 
@@ -61,41 +69,47 @@ var data_table = (dbresults) =>{
               time_to_bed: moment(found.down, "X").format("HH:mm"), 
               date_up_from_bed: moment(found.up, "X").format("YYYY-MM-DD"), 
               time_up_from_bed: moment(found.up, "X").format("HH:mm"), 
-              time_in_bed: seconds_to_text(seconds_in_bed),
-              sleep_rate: found.rate,
-              time_awake: seconds_to_text(found.awake),
-              time_asleep: seconds_to_text(seconds_asleep),
-              sleep_efficiency: ( seconds_asleep/seconds_in_bed*100 ).toString().split(".")[0] + '%'
+              time_in_bed: seconds_to_text(seconds_in_bed), 
+              sleep_rate: found.rate, 
+              time_awake: seconds_to_text(found.awake), 
+              time_asleep: seconds_to_text(seconds_asleep), 
+              seconds_asleep: seconds_asleep, 
+              t: found.t, 
+              sleep_efficiency: ( se ).toString().split(".")[0] + '%'
             }
-        } ); // push into array
-      }
+        } ); // push into array 
+
+        // SET/TEST this_week:
+        // { 22: {avg_sleep_efficiency: 0, avg_sleep_time: 0, se_arr: [], sleep_time_arr: [] } }
+        if( weeks[week_num] ){ 
+          weeks[week_num].sleep_time_arr.push(seconds_asleep);
+          weeks[week_num].se_arr.push(se);
+        } else {
+          weeks[week_num] = { se_arr: [se], sleep_time_arr: [seconds_asleep] };
+        } 
+
+
+      } // WHILE inner same day
     i++;
     this_up_date = moment(first_date_str).add(i, 'days').format("YYYY-MM-DD").toString();
     
   } // while 
 
-  const arrAvg = (accumulator, currentValue) => accumulator + currentValue;  
-
-  // Build Weeks object 
-  posts_table.map( (day)=>{ 
-    if(weeks.has(day.week) === false){ weeks.set(day.week, { avg: 0, posts: [] } ); } // set current week to 0 in map if doesnt exist
-    
-    if(day.found){ 
-      arr = weeks.get(day.week).posts;
-      arr.push( parseInt(day.data.sleep_efficiency) );
-      weeks.set( day.week, { posts: arr, avg: Math.round( arr.reduce(arrAvg)/arr.length) } ); 
-    } 
+  // Finish Weeks avg calculations
+  Object.keys(weeks).forEach( (n) => { 
+    week_arr.push({w: n, val: {
+      sleep_time_arr: weeks[n].sleep_time_arr, 
+      se_arr: weeks[n].se_arr,
+      avg_sleep_time: ((weeks[n].sleep_time_arr.reduce(arrSum) / weeks[n].sleep_time_arr.length)/60/60),
+      avg_sleep_efficiency: (weeks[n].se_arr.reduce(arrSum) / weeks[n].se_arr.length),
+    } });
   });
-
-  let weeks_arr = [];
-  weeks.forEach((v,k)=> weeks_arr.push({w: k, val: v}) );
   
   return { 
     data_table: posts_table,
     current_week: moment().format('ww'),
     first_week: first_week_str,
-    // weeks: Array.fromEntries( weeks )    // 
-    weeks: weeks_arr // <- turn Map into Obj
+    weeks: week_arr 
   }
   
 }
