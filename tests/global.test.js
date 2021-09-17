0b9538c82208fdc7457b2e008038561ffdd6d8cf
Johnny Ulinder <julinder@gmail.com>
2021-09-17 11:16:20 +0200

    initial test modules

---
+++ tests/global.test.js
@@ -0,0 +1,14 @@
+const request = require("supertest");
+const app = require(app_path);
+
+describe("Test the root path", () => {
+  test("It should response the GET method", done => {
+    request(app)
+      .get("/")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
